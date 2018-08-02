d079ce269ea9621228b3fd7ff1d5c07a6d98fc5a
jpriyanka515 <41731145+jpriyanka515@users.noreply.github.com>
2018-08-02 15:58:01 +0530

    Create program20.java

---
+++ program20.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,r,q;
+		Scanner S=new Scanner(System.in);
+		n=S.nextInt();
+		for(r=1;r<=5;r++)
+		{
+			q=n*r;
+			System.out.println(q);
+			
+		}
+	}
+}
