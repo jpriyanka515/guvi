b934089b9b4f95fb7fcab7d497380fb7f5623084
jpriyanka515 <41731145+jpriyanka515@users.noreply.github.com>
2018-08-14 10:47:07 +0530

    Update program41.java

--- program41.java
+++ program41.java
@@ -10,13 +10,14 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		String str;
-		int i,k;
+		int k,i;
 		Scanner s=new Scanner(System.in);
-		k=s.nextInt();
 		str=s.nextLine();
+		k=s.nextInt();
 		for(i=0;i<k;i++)
 		{
 			System.out.println(str);
 		}
+		
 	}
 }
