e5a96cded4c568dc7042269558510df37336ee9d
jpriyanka515 <41731145+jpriyanka515@users.noreply.github.com>
2018-08-14 11:20:41 +0530

    Create program42.java

---
+++ program42.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str1,str2;
+		int n1,n2;
+		Scanner S=new Scanner(System.in);
+		str1=S.nextLine();
+		str2=S.nextLine();
+		n1=str1.length();
+		n2=str2.length();
+		if(n1>n2)
+		{
+			System.out.println(str1);
+		}
+		else
+		{
+			System.out.println(str2);
+		}
+	}
+}
