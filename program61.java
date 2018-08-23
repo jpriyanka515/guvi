e67f38cd5885cf3694a8944965329187c98d47f4
jpriyanka515 <41731145+jpriyanka515@users.noreply.github.com>
2018-08-23 15:30:39 +0530

    Create program61.java

---
+++ program61.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str;
+		int k,i;
+		Scanner S=new Scanner(System.in);
+		str=S.next();
+		k=S.nextInt();
+		char[] ch=str.toCharArray();
+		for(i=0;i<k;i++)
+		{
+			System.out.println(ch[i]+"\t");
+		}
+	}
+}
