8a7fb043d3c3c886255fba3de0d95f628237f528
jpriyanka515 <41731145+jpriyanka515@users.noreply.github.com>
2018-08-24 10:16:04 +0530

    Update program79.java

--- program79.java
+++ program79.java
@@ -9,26 +9,19 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int n1,n2,p,i;
-		boolean flag=false;
-		Scanner S=new Scanner(System.in);
-		n1=S.nextInt();
-		n2=S.nextInt();
-		p=n1*n2;
-		for(i=1;i<p/2;i++)
-		{
-			if(p==i*i)
-			{
-				flag=true;
-			}
-		}
-		if(flag)
-		{
-			System.out.println("yes");
-		}
-		else
-		{
-			System.out.println("no");
-		}
+	 int n,m,r;
+	 Scanner S=new Scanner(System.in);
+	 n=S.nextInt();
+	 m=S.nextInt();
+	 r=n*m;
+	 int sqrt=(int)Math.sqrt(r);
+	 if(sqrt*sqrt==r)
+	 {
+	 	System.out.println("yes");
+	 }
+	 else
+	 {
+	 	System.out.println("no");
+	 }
 	}
 }
