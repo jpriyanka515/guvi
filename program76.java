fd2f84f12efcbbbf4966598584d84e3425352b0a
jpriyanka515 <41731145+jpriyanka515@users.noreply.github.com>
2018-08-27 15:02:52 +0530

    Create program76.java

---
+++ program76.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int n,i;
+	 Scanner s=new Scanner(System.in);
+	 n=s.nextInt();
+	 boolean flag=false;
+	 for(i=2;i<n;i++)
+	 {
+	 	if(n%i==0)
+	 {
+	 	flag=true;
+	 }
+	}
+	if(flag)
+	{
+		System.out.println("yes");
+	}
+	else
+	{
+		System.out.println("no");
+	}
+}
+}
