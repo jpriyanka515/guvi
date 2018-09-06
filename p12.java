5a5f085753e63426ed22e4c9f50bb448e12c4604
jpriyanka515 <41731145+jpriyanka515@users.noreply.github.com>
2018-09-06 12:34:08 +0530

    Update p12.java

--- p12.java
+++ p12.java
@@ -2,16 +2,19 @@ import java.util.*;
 import java.lang.*;
 import java.util.ArrayList;
  class rotat {
-     public static void main(String aa[]){
+     public static void main(String aa[])
+     {
      int n;
      Scanner s=new Scanner(System.in);
      n=s.nextInt();
      int k=s.nextInt();
      ArrayList<Integer>a=new ArrayList<Integer>();
-     for(int i=0;i<n;i++){
+     for(int i=0;i<n;i++)
+     {
         int c=s.nextInt();
         a.add(c);
      }
+      
      Collections.rotate(a,k );
      for(int d:a){
          System.out.printf("%d ", d);
