a0115491601af6a5e63c1aefe1f0769a56181381
jpriyanka515 <41731145+jpriyanka515@users.noreply.github.com>
2018-09-10 16:11:57 +0530

    Create p60.java

---
+++ p60.java
@@ -0,0 +1,38 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str1,str2;
+		int i,n,p=0,j;
+		Scanner s=new Scanner(System.in);
+		str1=s.next();
+		str2=s.next();
+		n=str1.length();
+		for(i=0;i<n;i++)
+		{
+	              for(j=i+1;j<n;j++)
+	              {
+                  if(str1.charAt(i)==str2.charAt(j))
+                  {
+                  	p++;
+                  	break;
+                  }
+		}
+		}
+		if(p==1)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
